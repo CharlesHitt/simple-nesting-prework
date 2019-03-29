0519af8c2a9dee2b79907086a4da71073d6918f7
Charles Hitt <charles.a.hitt@gmail.com>
2019-03-29 01:07:52 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,7 +81,10 @@ def adding_matz
           :languages => ["C"]
         }
      }
-programmer_hash.unshift(yukihiro_matsumoto => {{:known_for => "Ruby"},{:languages => "Lisp, C"
+programmer_hash.unshift(
+  yukihiro_matsumoto => {
+    :known_for => "Ruby", 
+    :languages => ["Lisp, C"]
 })
     
 end
